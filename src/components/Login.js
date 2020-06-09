57db8fdf14633d602f18bd490248fef4f5e4165a
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-06-09 10:19:50 -0700

    Working registration

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Button, Container, TextField } from '@material-ui/core';
-
+import "../css/register.css";
 import { sendLoginReq } from '../redux/user';
 
 const Login = (props) => {
@@ -25,13 +25,38 @@ const Login = (props) => {
   }
 
   return (
-    <Container>
-      <div>Username:</div>
-      <TextField id="outlined-basic" label="Username" variant="outlined" onChange={userNameChange} />
-      <div>Password:</div>
-      <TextField id="outlined-basic" label="Password" variant="outlined" type="password" onChange={passwordChange} />
-      <Button color="primary" onSubmit={loginUser}>Submit</Button>
-    </Container>
+    // <Container>
+    <div className="wrapper">
+      <div className="form-wrapper">
+        <h1>Log In</h1>
+        <form>
+          <div className="userName">
+            <div>Username:</div>
+            <TextField
+              id="outlined-basic"
+              label="Username"
+              variant="outlined"
+              onChange={userNameChange}
+            />
+          </div>
+          <div className="password">
+            <div>Password:</div>
+            <TextField
+              id="outlined-basic"
+              label="Password"
+              variant="outlined"
+              type="password"
+              onChange={passwordChange}
+            />
+          </div>
+          <div className="logIn">
+            <Button color="primary" onSubmit={loginUser}>Submit</Button>
+            <small>Create An Acount</small>
+          </div>
+        </form>
+      </div>
+    </div>
+    // </Container>
   )
 }
 
---
+++ src/config.js
@@ -0,0 +1,3 @@
+const production = process.env.NODE_ENV === "production";
+export const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+// export const apiBaseUrl = production ? "edit later" : process.env.REACT_APP_API_BASE_URL;

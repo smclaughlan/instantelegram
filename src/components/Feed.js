1cb7b309107d3eeaf70a11ee672ebac5d204d73f
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-07-15 15:46:37 -0700

    Fix order of images/posts on feed and profile pages

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -51,47 +51,55 @@ function Feed(props) {
     props.getUserProfileReq(id);
   }, []);
 
+  if (props.feedPosts) {
+    props.feedPosts.sort((a, b) => {
+      if (a.timestamp > b.timestamp) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+  }
+  const classes = useStyles();
+  return (
+    (props.feedPosts && props.user) ?
+      <div className={classes.root}>
+        {window.screen.width > 960 ? (
+          <Container className={classes.topLogo}>
+            <img alt={"Instantelegram logo"} src={"images/logoIG.png"} style={{ margin: '32px auto', borderRadius: '5px', maxWidth: '950px' }}></img>
+          </Container>) : <></>
+        }
 
-    const classes = useStyles();
-    return (
-        (props.feedPosts && props.user) ?
-            <div className={classes.root}>
-                {window.screen.width > 960 ? (
-                    <Container className={classes.topLogo}>
-                        <img alt={"Instantelegram logo"} src={"images/logoIG.png"} style={{ margin: '32px auto', borderRadius: '5px', maxWidth: '950px' }}></img>
-                    </Container>) : <></>
-                }
-
-                <Paper className={classes.paper}>
-                    <Grid
-                        container
-                        spacing={3}
-                        direction="column"
-                        justify="center"
-                        alignContent="center"
-                        alignItems="flex-start"
-                    >
-                        {props.feedPosts.map((post) => {
-                            return (
-                                <Grid item className={classes.column1}>
-                                    <Image
-                                        imageId={post.postId}
-                                        postDate={post.timestamp}
-                                        imageUrl={post.imageUrl}
-                                        imageCapt={post.caption}
-                                        imagePosterUsername={post.username}
-                                        imagePosterAviUrl={post.avatarUrl}
-                                        imagePosterId={post.userId}
-                                    />
-                                </Grid>
-                            )
-                        })}
-                    </Grid>
-                </Paper>
-            </div >
-            :
-            <CircularProgress />
-    )
+        <Paper className={classes.paper}>
+          <Grid
+            container
+            spacing={3}
+            direction="column"
+            justify="center"
+            alignContent="center"
+            alignItems="flex-start"
+          >
+            {props.feedPosts.reverse().map((post) => {
+              return (
+                <Grid item className={classes.column1}>
+                  <Image
+                    imageId={post.postId}
+                    postDate={post.timestamp}
+                    imageUrl={post.imageUrl}
+                    imageCapt={post.caption}
+                    imagePosterUsername={post.username}
+                    imagePosterAviUrl={post.avatarUrl}
+                    imagePosterId={post.userId}
+                  />
+                </Grid>
+              )
+            })}
+          </Grid>
+        </Paper>
+      </div >
+      :
+      <CircularProgress />
+  )
 
 }
 

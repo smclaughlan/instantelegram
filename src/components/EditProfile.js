717320fd3d82da4ddc56283c8bb6bce4089d6a34
Riki Kaneshiro <arkaneshiro@gmail.com>
2020-12-01 16:48:15 -0800

    started edit profile refactor

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,55 +1,56 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Button, Container, TextField, Input, InputLabel } from '@material-ui/core';
-import { makeStyles } from "@material-ui/core/styles";
+// import { makeStyles } from "@material-ui/core/styles";
 
 import { updateAvatar, updateBioReq } from '../redux/editProfile';
 
-const useStyles = makeStyles(theme => ({
-  bioUpdate: {
-    width: "100%",
-  },
-}));
+// const useStyles = makeStyles(theme => ({
+//   bioUpdate: {
+//     width: "100%",
+//   },
+// }));
 
 const EditProfile = (props) => {
   const [bio, setBio] = useState('')
-  const classes = useStyles();
+  // const classes = useStyles();
 
   const updateValue = cb => e => cb(e.target.value);
 
   const handleNewImage = e => {
     const newImg = e.target.files[0];
-    let userId = window.location.href.split("/")[4];
-    props.updateAvatar(userId, newImg, props.token);
+    props.updateAvatar(props.currentUserId, newImg, props.token);
   }
 
   const updateBio = e => {
     e.preventDefault();
-    let userId = window.location.href.split("/")[4];
-    props.updateBioReq(userId, bio, props.token)
+    props.updateBioReq(props.currentUserId, bio, props.token)
   }
 
   return (
-    <Container style={{ display: 'grid', gridGap: '32px' }}>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <div style={{ alignSelf: 'center' }}>Image Preview:</div>
-        <img src={props.avatarUrl} alt='preview' width="400" style={{ alignSelf: 'center' }} />
-        <InputLabel style={{ alignSelf: 'center' }} htmlFor="image-upload" >Select Image</InputLabel>
+    <Container>
+      <div>
+        <InputLabel htmlFor="image-upload" >
+          <img src={props.avatarUrl} alt='preview' width="100" />
+          <div>select new image</div>
+        </InputLabel>
         <Input id="image-upload" type="file" label="Image" style={{ display: 'none' }} onChange={handleNewImage} />
       </div>
+
       <div>
         <div>Bio:</div>
         <TextField
           variant="outlined"
           type="caption"
           onChange={updateValue(setBio)}
-          className={classes.bioUpdate}
+          // className={classes.bioUpdate}
           defaultValue={props.profileBio}
         />
         <div>
           <Button color="primary" onClick={updateBio} >Update bio</Button>
         </div>
       </div>
+
     </Container>
   )
 }
@@ -57,21 +58,16 @@ const EditProfile = (props) => {
 const mapStateToProps = state => {
   return {
     token: state.user.token,
+    currentUserId: state.user.currentUserId,
     avatarUrl: state.user.profile.avatarUrl,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    // TODO: update this with token to lock it down
     updateBioReq: (...args) => dispatch(updateBioReq(...args)),
     updateAvatar: (...args) => dispatch(updateAvatar(...args)),
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(
-  EditProfile
-);
+export default connect(mapStateToProps, mapDispatchToProps)(EditProfile);
